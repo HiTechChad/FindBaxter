bd33cbced833116017bf055dc73619f637f5a89f
Rob Korobkin <rob.korobkin@gmail.com>
2016-12-15 23:21:46 -0500

    Moving config file out of repo.

--- config/config.py
+++
@@ -1,9 +0,0 @@
-
-import mysql.connector, os
-
-# database
-conn = mysql.connector.connect(
-         user='root',
-         host='127.0.0.1',
-         password='root',
-         database='sandbox')
--- mysql_example/database.py
+++ mysql_example/database.py
@@ -34,7 +34,7 @@ def get_row(query) :
 
 
 
-# RUN QUERY AND RETURN DATA OBJECT
+# RUN QUERY AND RETURN LIST
 def get_results(query) :
 
 	# Run Query
