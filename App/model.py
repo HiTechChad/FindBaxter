a32a0de70125b4ac0ab56cae7f5f037872b1c18b
Dawson <Spencer>
2016-12-16 14:03:49 -0500

    update

---
+++ App/model.py
@@ -0,0 +1,33 @@
+import database
+
+class AppModel:
+
+	def getPerson(model, request):
+		name = request["name"]
+		query = "SELECT * FROM users where name='{}' LIMIT 1".format(name)
+		return database.get_row(query)
+		
+
+	def addPerson(model, request):
+		if (!("person" in request)) :
+			return {"error" : "Request Error: No person requested"}
+		person = request["person"]
+		return database.insertObj(person, "users")
+
+		
+	'''
+	getPeople
+	- @param:
+		// if no param, return all people
+		
+	- @return PEOPLE LIST
+		[
+			{ person (see above)},
+			{ person (see above)}
+	]
+	'''
+	def getPeople(model, request):
+		query = "SELECT * FROM users"
+		return database.get_results(query)
+		
+		
