d710ca9bad0dea23852e016ac7aa06397fc6d8f9
Dawson <Spencer>
2017-01-06 08:26:26 -0500

    twililo testing

--- Twilio/twillow.py
+++ Twilio/twillow.py
@@ -5,10 +5,8 @@ account_sid = "ACd37c9f284658c4b14ed1878a1b4f7feb" # Your Account SID from www.t
 auth_token  = "d4338b36ba002e27a41ddd4c3452bfce"  # Your Auth Token from www.twilio.com/console
 
 client = TwilioRestClient(account_sid, auth_token)
-
+print "Attempting to send message"
 try:
-    message = client.messages.create(body="Hello from Python",
-        to="+12079290417",    # Replace with your phone number
-        from_="+17865654843 ") # Replace with your Twilio number
+    message = client.messages.create(body="Hello from Python", to="+12079290417", from_="+17865654843") # Replace with your Twilio number
 except TwilioRestException as e:
     print(e)
