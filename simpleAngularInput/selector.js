c764a60abd2e2e1d4f407c87b597f05445898bba
Rob Korobkin <rob.korobkin@gmail.com>
2017-05-12 11:10:49 -0400

    Adding comments to Angular tutorial

--- simpleAngularInput/selector.js
+++ simpleAngularInput/selector.js
@@ -1,16 +1,17 @@
 
 
-
+/* START ANGULAR APP */
 var app = angular.module('selectorApp', []);
-
 app.controller('selectorCtrl', ['$scope', 
 	function($scope){
 
 
+
+		/* EXAMPLE CODE */
+
 		$scope.person = {
 			name : "What is your name?"
 		}
-
 
 		$scope.changeName = function(){
 			$scope.person.name = 'Dawson'
